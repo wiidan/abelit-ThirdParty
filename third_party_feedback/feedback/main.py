516f0f509d2536c46753a8b2633a61637d5a7b2d
Abelit <ychenid@live.com>
2019-12-25 18:00:00 +0800

    add dce data

--- third_party_feedback/feedback/main.py
+++ third_party_feedback/feedback/main.py
@@ -37,7 +37,8 @@ def init_db():
     for table in result:
         conn.execute("drop table {0}".format(table[0]))
 
-    SQL_TEXT = ["create table tto_question(id integer primary key autoincrement, presentation text,type text, name text,block integer,source_text text)","create table interviewer(id integer, name text)"]
+    SQL_TEXT = ["create table tto_question(id integer primary key autoincrement, presentation text,type text, name text,block integer,source_text text)",
+                "create table interviewer(id integer, name text)", "create table dce_question(id integer primary key autoincrement,presentation text,name text,block integer,answer text,source_text text)"]
 
     for sql in SQL_TEXT:
         conn.execute(sql)
@@ -49,6 +50,11 @@ def init_db():
 
     for data in readexcel.read('./data/questions.xlsx','TTO & TTO-Feedback', True):
         cursor.execute("insert into tto_question(presentation,type,name,block,source_text) values('{0}','{1}','{2}','{3}','{4}')".format(data[0],data[1],data[2],data[3],data[4]))
+    conn.commit()
+
+    for data in readexcel.read('./data/questions.xlsx', 'DCE', True):
+        cursor.execute("insert into dce_question(presentation,name,block,answer,source_text) values('{0}','{1}','{2}','{3}','{4}')".format(
+            data[0], data[1], data[2], data[3], data[4]))
     conn.commit()
 
     result = cursor.execute(all_table_text)
@@ -79,6 +85,28 @@ def get_tto_question():
 
     for row in result:
         data.append({"id": row[0], "presentation": row[1], "type": row[2], "name": row[3], "block": row[4], "source_text": row[5]})
+
+    return jsonify(data)
+
+
+@app.route("/api/question/dce")
+def get_dce_question():
+    block = request.args.get('block')
+    print(block)
+    # 连接数据库
+    conn = sqlite3.connect('question.db', check_same_thread=False)
+    cursor = conn.cursor()
+
+    SQL_TEXT = "select id,presentation,name,block,answer,source_text from dce_question where block='{0}'".format(
+        block)
+
+    result = cursor.execute(SQL_TEXT)
+
+    data = []
+
+    for row in result:
+        data.append({"id": row[0], "presentation": row[1], "name": row[2],
+                     "block": row[3], "answer": row[4], "source_text": row[5]})
 
     return jsonify(data)
 
