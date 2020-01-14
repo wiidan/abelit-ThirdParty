f196e1246171be9fdcd7aecf40795521fcefed2e
Abelit <ychenid@live.com>
2020-01-14 16:13:52 +0800

    update backend url

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,7 +14,7 @@ import "@mdi/font/css/materialdesignicons.css";
 
 // import axios for ajax application
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5000/";
+axios.defaults.baseURL = "http://58.42.231.98:5000/";
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 Vue.prototype.$axios = axios;
 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   // config
   assetsDir: "static", // For simple configuration of static files in Flask (the "static_folder='client/dist/static'" part in app.py)
   devServer: {
-    proxy: 'http://localhost:5000/',// So that the client dev server can access your Flask routes
+    proxy: 'http://58.42.231.98:5000/',// So that the client dev server can access your Flask routes
 
     // host: 'localhost',
     // port: 8080,
